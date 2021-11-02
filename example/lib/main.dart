96c20a827f2503866713eef6a574eb1bd6e5de46
Ersikan <julien.philippon@epitech.eu>
2021-11-02 19:44:01 +0100

    feat: Use ReviewForm in the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:review/review.dart';
 
-List<Review> get exampleReviews => [
-      Review(
-          reviewerName: "Bob",
-          reviewContent: "This is great !",
-          mark: 4,
-          date: DateTime.now()),
-      Review(
-          reviewerName: "Bob",
-          reviewContent:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          mark: 5,
-          date: DateTime.now()),
-      Review(
-          reviewerName:
-              "A veryyyyyyyyy lonnnnnnnnnnnnnnnnnnnnnnng reviewerName",
-          reviewContent: "reviewContent",
-          mark: 0,
-          date: DateTime(2021, 01, 01))
-    ];
+final List<Review> exampleReviews = [
+  Review(
+      reviewerName: "Bob",
+      reviewContent: "This is great !",
+      mark: 4,
+      date: DateTime.now()),
+  Review(
+      reviewerName: "Bob",
+      reviewContent:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      mark: 5,
+      date: DateTime.now()),
+  Review(
+      reviewerName: "A veryyyyyyyyy lonnnnnnnnnnnnnnnnnnnnnnng reviewerName",
+      reviewContent: "reviewContent",
+      mark: 0,
+      date: DateTime(2021, 01, 01))
+];
 void main() {
   runApp(const MyApp());
 }
@@ -39,8 +38,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<Review> _reviews = exampleReviews;
 
   @override
   Widget build(BuildContext context) {
@@ -48,11 +54,23 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Example usage of Review"),
       ),
-      body: ListView(
-        children: exampleReviews
-            .map((review) => ReviewDisplay(review: review))
-            .toList(),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: exampleReviews
+                .map((review) => ReviewDisplay(review: review))
+                .toList(),
+          )),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final review = await showReviewForm(context);
+            if (review != null) {
+              setState(() {
+                _reviews.add(review);
+              });
+            }
+          },
+          child: const Icon(Icons.add)),
     );
   }
 }
