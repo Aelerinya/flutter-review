f62103bdb0d12749e1fc7eb73e9b34abc49231fa
Ersikan <julien.philippon@epitech.eu>
2021-11-02 19:51:06 +0100

    test: Basic unit test

--- test/review_test.dart
+++ test/review_test.dart
@@ -3,10 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:review/review.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Review toString', () {
+    final review = Review(
+        reviewerName: "Bob",
+        reviewContent: "My review",
+        mark: 3,
+        date: DateTime(2000));
+    expect(
+        review.toString(),
+        equals(
+            "Review of 3 out of 5 by Bob on 2000-01-01 00:00:00.000 : My review"));
   });
 }
